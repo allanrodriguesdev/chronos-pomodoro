12a662b8bdef1996a46e49325b171242813c0292
Allan Rodrigues <dmenorhc@hotmail.com>
2024-05-02 08:45:30 +0000

    aula 22

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { Heading } from './components/Heading';
+
+
 import './styles/thema.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
+
 
 export function App() {
-    console.log('oiiii');
     return (
         <>
-            <h1>Olá Mundo!</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea reprehenderit 3
-                excepturi ab totam est vero earum consectetur eaque obcaecati alias pariatur
-                delectus minima corporis vitae, adipisci nam quod. Voluptatem, quod?
-            </p>
+            <Container>
+                <Logo />
+            </Container>
+            <Container>
+                <Heading>MENU</Heading>
+            </Container>
+
+            
         </>
     );
 }
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+type LogoProps = {
+    children: React.ReactNode;
+}
+
+export function Logo() {
+    
+    return <div className={styles.logo}>
+        <a href="" className={styles.logoLink}>
+            <TimerIcon size={64}/>
+            <span>Chronos</span>
+        </a>
+    </div>;
+}
